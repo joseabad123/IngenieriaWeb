dd1dffffbfa215aaeec385eb9a0a27d9cd8e79b9
Jose Abad <jmabad2@utpl.edu.ec>
2020-07-09 22:42:47 -0500

    calculo de valores

--- taller js/app3/script.js
+++ taller js/app3/script.js
@@ -1,6 +1,6 @@
 var nombre = "José";
 var apellido = "Abad";
-let subtotal = 500;
+let subtotal = 0;
 var descuento;
 const iva = 0.12; // declarar constantes
 var nombrescompletos= nombre+" "+apellido;
@@ -8,7 +8,7 @@ var nombrescompletos= nombre+" "+apellido;
 var datos= document.getElementById("Cliente");
 var datos2= document.getElementById("Articulos");
 var datos3= document.getElementById("Valores");
-var random=document.getElementById("Random");
+var random=document.getElementById("NumFactura");
 let ran = Math.ceil(Math.random()*100);
 
 //datos encabezado de factura
@@ -16,25 +16,41 @@ random.innerHTML=`Factura N° 00${ran}`;
 datos.innerHTML=`<h2> Nombre del cliente: ${nombre} ${apellido}</h2>`;//Incrustar datos en string
 
 //Detalle de productos
-var productos =['Arroz','Azucar','Harina','Sal','Avena'];
+var productos =[
+                ['Arroz', 5, 12],
+                ['Azucar', 4, 8],
+                ['Harina', 2, 4],
+                ['Sal', 2, 3],
+                ['Avena', 3, 6]
+                            ];
+
 datos2.innerHTML += "<h3> Detalle de productos</h3>";
+datos2.innerHTML = datos2.innerHTML + (productos[0][0] + "<br> Cantidad: " + productos[0][1] +" kilos <br> Valor: $"+ productos[0][2] +"<br>"+ 
+    "<br>" + productos[1][0] + "<br> Cantidad: " + productos[1][1] +" kilos <br> Valor: $"+ productos[1][2] +"<br>"+ 
+    "<br>" + productos[2][0] + "<br> Cantidad: " + productos[2][1] +" kilos <br> Valor: $"+ productos[2][2] +"<br>"+ 
+    "<br>" + productos[3][0] + "<br> Cantidad: " + productos[3][1] +" kilos <br> Valor: $"+ productos[3][2] +"<br>"+ 
+    "<br>" + productos[4][0] + "<br> Cantidad: " + productos[4][1] +" kilos <br> Valor: $"+ productos[4][2] +"<br>"+ 
+    "</br>");
+// console.log(productos[0][2]);
 
-for(let x=0;x<productos.length; x++){
-    datos2.innerHTML += "<ul><li>"+productos[x]; "</li></ul>"
-    
-}
-
-// otra forma de for
+// otra forma de for 
 // productos.forEach((producto)=>{
 //     document.write(producto+"<br>");
 // });
 
 //Cálculo de valores
-if (subtotal>=200) {
-    datos3.innerHTML="Su monto es mayor a $200 ";
+
+subtotal =  productos[0][2] +
+            productos[1][2] +
+            productos[2][2] +
+            productos[3][2] +
+            productos[4][2] ;
+
+if (subtotal>=20) {
+    datos3.innerHTML=`Su monto (${subtotal}) es mayor a $20`;
     descuento =0.10;
-}else{
-    datos3.innerHTML="Su monto es menor a $199 ";
+} else {
+    datos3.innerHTML="Su monto es menor a $19 ";
     descuento =0.05;
 }
 
@@ -46,8 +62,8 @@ function calculoIva(){
 function imprimir(){
     var resIva=calculoIva(subtotal);
     var total=subtotal+resIva;
-    datos3.innerHTML +="<br>Su valor de iva a pagar es:"+resIva;
-    datos3.innerHTML +="<br>Su valor total de la factura es:"+total;
+    datos3.innerHTML +="<br>Su valor de iva a pagar es: " + resIva;
+    datos3.innerHTML +="<br>Su valor total de la factura es: " + total;
 }
 
 imprimir();
